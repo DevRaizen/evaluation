01cd4fd9b8a64e7a021e4225a53f12986e7bdddf
devRaizen <shawnbulos03@gmail.com>
2026-07-15 01:44:02 +0800

    updated admin dashboard

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SharedService {
-  defaultAvatar = "/user.png";
+  defaultAvatar = "user.png";
 
   Student: {
     Fname?: string;
